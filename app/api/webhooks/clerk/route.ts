216d48f7283d8305e31c14c1ed22e0c4d03ade25
Kenny <dohoainam0804@gmail.com>
2025-06-28 22:30:14 +0700

    Updated key

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,5 +1,7 @@
+import { createUser, deleteUser, updateUser } from "@/lib/actions/user.actions";
+import { clerkClient } from "@clerk/nextjs/server";
 import { verifyWebhook } from "@clerk/nextjs/webhooks";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   try {
@@ -9,6 +11,65 @@ export async function POST(req: NextRequest) {
     // For this guide, log payload to console
     const { id } = evt.data;
     const eventType = evt.type;
+    // CREATE
+    if (eventType === "user.created") {
+      const {
+        id,
+        email_addresses,
+        image_url,
+        first_name,
+        last_name,
+        username,
+      } = evt.data;
+
+      const user = {
+        clerkId: id,
+        email: email_addresses[0].email_address,
+        username: username || "",
+        firstName: first_name || "",
+        lastName: last_name || "",
+        photo: image_url || "",
+      };
+
+      const newUser = await createUser(user);
+      const clerk = await clerkClient();
+
+      // Set public metadata
+      if (newUser) {
+        clerk.users.updateUserMetadata(id, {
+          publicMetadata: {
+            userId: newUser._id,
+          },
+        });
+      }
+
+      return NextResponse.json({ message: "OK", user: newUser });
+    }
+
+    // UPDATE
+    if (eventType === "user.updated") {
+      const { id, image_url, first_name, last_name, username } = evt.data;
+
+      const user = {
+        firstName: first_name || "",
+        lastName: last_name || "",
+        username: username || "",
+        photo: image_url || "",
+      };
+
+      const updatedUser = await updateUser(id, user);
+
+      return NextResponse.json({ message: "OK", user: updatedUser });
+    }
+
+    // DELETE
+    if (eventType === "user.deleted") {
+      const { id } = evt.data;
+
+      const deletedUser = await deleteUser(id!);
+
+      return NextResponse.json({ message: "OK", user: deletedUser });
+    }
     console.log(
       `Received webhook with ID ${id} and event type of ${eventType}`
     );
